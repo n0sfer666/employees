497e1ba1d2411a582f319d126a35ea30cd5a0d74
Khasanov Petr <n0sfer666@yandex.ru>
2022-08-31 15:06:52 +0300

    refactored table-employees

--- src/components/table-employees/table-employees.tsx
+++ src/components/table-employees/table-employees.tsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { MouseEvent } from 'react'
+import { useNavigate } from 'react-router-dom'
 import TableHead from '../table-head/table-head'
 import { ITableEmployeesProps } from './table-employees.types'
 
 function TableEmployees ({ employees }: ITableEmployeesProps): JSX.Element {
+  const navigate = useNavigate()
+  const handleTrClick = (event: MouseEvent<HTMLTableRowElement>): void => {
+    event.stopPropagation()
+    navigate(`/employee-edit/${event.currentTarget.id}`)
+  }
   return (
     <table className='table table-striped table-borderless table-info'>
       <TableHead titles={Object.keys(employees[0])} />
       <tbody>
         {
           employees.map((employee) => (
-            <tr key={employee.id}>
+            <tr
+              className='clickable'
+              id={employee.id.toString()}
+              key={employee.id}
+              onClick={handleTrClick}
+            >
               {
                 Object.keys(employee).map((key) => (
                   key === 'id'
