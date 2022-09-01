280331bb6b944515731f5e2f39cfc6e5154e5e17
Khasanov Petr <n0sfer666@yandex.ru>
2022-09-01 11:54:48 +0300

    added windowWidth checker for responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import TableEmployees from '../components/table-employees/table-employees'
 import LayoutMain from '../layout/main'
 import { TRootState } from '../store/store.types'
 import { IEmployee } from '../types/employee.types'
 
+const maxWidthForTable = 768
+
+function getWindowWidth (): number {
+  return window.innerWidth
+}
+
 function PageIndex (): JSX.Element {
+  const [windowsWidth, setWindowWidth] = useState(getWindowWidth())
   const employees: IEmployee[] = useSelector((state: TRootState) => state.employees.list)
+  useEffect(() => {
+    function handleWindowResize (): void {
+      setWindowWidth(getWindowWidth())
+    }
+    window.addEventListener('resize', handleWindowResize)
+  }, [])
   return (
     <LayoutMain
       header={'Сотрудники'}
