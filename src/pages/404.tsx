3d960cfe65230585b6ae4d5cad04db9c9b05dcbe
Khasanov Petr <n0sfer666@yandex.ru>
2022-09-02 09:17:46 +0300

    jesus christ! 😒

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Page404 (): JSX.Element {
+  const navigate = useNavigate()
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/')
+    }, 2000)
+  }, [])
   return (
     <h1 className='h1 text-center text-red-800'> Такой страницы нет... и не будет</h1>
   )
