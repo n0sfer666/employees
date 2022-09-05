6d135ea0c10809c2248c1f1106ea6be07ac4cce2
Khasanov Petr <n0sfer666@yandex.ru>
2022-09-05 13:24:40 +0300

    refactored app

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -6,16 +6,14 @@ import { initEmployees } from '../store/slices/employees-data'
 import { TRootState } from '../store/store.types'
 import { TRoles } from '../types/employee.types'
 
+const initData = defaultData.map((employee) => {
+  return { ...employee, role: employee.role as TRoles }
+})
+
 function App (): JSX.Element {
   const dispatch = useDispatch()
   useEffect(() => {
-    dispatch(
-      initEmployees(
-        defaultData.map((employee) => {
-          return { ...employee, role: employee.role as TRoles }
-        })
-      )
-    )
+    dispatch(initEmployees(initData))
   }, [])
 
   const isInitData = useSelector((state: TRootState) => state.employees.isInit)
